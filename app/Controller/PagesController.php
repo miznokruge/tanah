59731f2be7feed73354bd62efd1e069c057cba91
Minzo <mizno.kruge@gmail.com>
2017-08-29 20:48:57 +0700

    init tanah

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -46,6 +46,10 @@ class PagesController extends AppController {
  *   or MissingViewException in debug mode.
  */
 	public function display() {
+        
+        
+        return $this->redirect('/users');
+        
 		$path = func_get_args();
 
 		$count = count($path);
